1e42a968766b10605726052cf61dd737ef550b31
Susantha Kumara <susanthaboss@gmail.com>
2019-07-22 09:04:33 +0530

    first commit

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import Vue from "vue";
+import BootstrapVue from 'bootstrap-vue';
 import App from "./App.vue";
 import router from "./router";
 import "./registerServiceWorker";
 
 Vue.config.productionTip = false;
+Vue.use(BootstrapVue);
 
 new Vue({
   router,
